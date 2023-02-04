a01323d445d6204781e32ff92eb91d4d2bab0987
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-04 19:55:55 +0530

    feat: return ip addresses of connected clients

--- backend/helpers/onClientConnected.js
+++ backend/helpers/onClientConnected.js
@@ -1,6 +1,6 @@
 const getIpAddress = require('./getIpAddress');
 
-const onClientConnected = (io, socket) => {
+const onClientConnected = async (io, socket) => {
   // get the ip address of the client to join him to the room
   // that matches his ip address
   const address = getIpAddress(socket);
@@ -11,8 +11,16 @@ const onClientConnected = (io, socket) => {
   // get the current number of clients in the room and return
   // this data back to the client
   const numberOfClients = io.sockets.adapter.rooms.get(address)?.size ?? 0;
+  const clientSockets = await io.in(address).fetchSockets();
+  const clientIps = clientSockets.map((socket) => {
+    return getIpAddress(socket);
+  });
+
+  // Kept here for testing. To be deleted later.
+  console.log({ clientIps });
+
   socket.emit('CONNECTION_SUCCESSFUL', {
-    numberOfClients: numberOfClients
+    clients: clientIps
   });
 };
 
