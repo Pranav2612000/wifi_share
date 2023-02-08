a70ecdddca7d5eab2548cb162b81f2b20aa7b67a
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-08 19:55:10 +0530

    feat: create debounced event handler to trigger when user stops typing

--- www/src/components/Scratchpad.jsx
+++ www/src/components/Scratchpad.jsx
@@ -2,10 +2,23 @@ import { useEffect, useState } from "react";
 
 import Loader from './Loader.jsx';
 import DiscoveryService from "../service/Discovery";
+import debounce from "../service/Debounce.js";
+
+const getStatusElement = (isUpdating) => {
+  if (isUpdating) {
+    return (
+      <p className='text-sm status-label' data-status='default'>Autosaving...</p>
+    )
+  }
+  return (
+    <p className='text-sm status-label' data-status='default'>Paste your text here</p>
+  )
+};
 
 const Scratchpad = () => {
   const [ peer, setPeer ] = useState(null);
   const [ loading, setLoading ] = useState(true);
+  const [ updating, setUpdating ] = useState(false);
 
   useEffect(() => {
     const peer = new DiscoveryService({
@@ -20,6 +33,19 @@ const Scratchpad = () => {
     }
   }, []);
 
+  const debouncedOnTextUpdate = debounce((e) => {
+    onTextUpdate(e);
+  });
+
+  const onTextUpdate = (e) => {
+    setUpdating(true);
+
+    /* To check if autosaving state updates are working as expected */
+    setTimeout(() => {
+      setUpdating(false);
+    }, 2000);
+  };
+
   return (
     <main className='scratchpad-container'>
       {loading ? (
@@ -27,11 +53,15 @@ const Scratchpad = () => {
       ) : (
         <>
           <section className='input-container'>
-            <textarea className='scratchpad-input text-md' placeholder='Paste something here'>
+            <textarea
+              className='scratchpad-input text-md'
+              placeholder='Paste something here'
+              onKeyUp={(e) => debouncedOnTextUpdate(e)}
+            >
             </textarea>
           </section>
           <section className='controls-container'>
-            <p className='text-sm status-label' data-status='default'>Paste your text here</p>
+            {getStatusElement(updating)}
             <div>
               <button className='text-sm success'>Save</button>
               <button className='text-sm failure'>Clear</button>
