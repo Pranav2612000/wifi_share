7d6236e771b10c8cec0791a4abde2b4378117273
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-12 15:17:09 +0530

    improv: use x-forwarded-for header to get external public ip addr

--- backend/helpers/getIpAddress.js
+++ backend/helpers/getIpAddress.js
@@ -1,6 +1,6 @@
 const getIpAddress = (socket) => {
   console.log("Headers", socket.handshake.headers);
-  return socket.request.connection.remoteAddress;
+  return socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
 }
 
 module.exports = getIpAddress;
