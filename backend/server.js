d2be3e90031b7902f695bc48c63a6cf9e823e97e
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-12 08:59:53 +0530

    feat: add handler on server for 'REQUEST_TEXT' event

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,38 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected', getIpAddress(socket));
   });
+
+  // Triggered when some client is asking for text
+  socket.on('REQUEST_TEXT', async (client) => {
+
+    // Kept here for testing. To be deleted later.
+    console.log('Client', client, 'called REQUEST_TEXT');
+
+    const clientSockets = await io.in(address).fetchSockets();
+
+    // If no other sockets exist return empty text back
+    if (!Array.isArray(clientSockets) || clientSockets.length <= 1) {
+      socket.emit('NEW_TEXT', {
+        text: ""
+      });
+      return;
+    }
+
+    // Otherwise fetch the latest text from the first client and use that
+    io.to(clientSockets[0].id).emit('REQUEST_TEXT', null, function (resp) {
+      // If there's an error in getting data we send back empty string
+      if(!resp || !resp.text) {
+        console.log("Recvd malformed data");
+        socket.emit('NEW_TEXT', {
+          text: ""
+        });
+      }
+
+      socket.emit('NEW_TEXT', {
+        text: resp.text
+      });
+    });
+  });
 });
 
 server.listen(port, () => console.log(`App started on port ${port}`));
