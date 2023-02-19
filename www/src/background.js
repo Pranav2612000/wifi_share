44ec9b8ab27a879d66d1a6f70e3a44fb6a82b9e6
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-19 13:22:08 +0530

    temp: playing with contexts

--- www/src/background.js
+++ www/src/background.js
@@ -1,3 +1,5 @@
+/*global chrome*/
+
 import DiscoveryService from "./service/Discovery";
 
 console.log('Starting discovery service in background');
@@ -16,3 +18,30 @@ const peer = new DiscoveryService({
   }
 });
 console.log({ peer });
+
+console.log({ chrome });
+console.log("action", chrome.action);
+chrome.action.setBadgeText(
+  { text: "ON" },
+  (err, resp) => { console.log({err, resp}) }
+);
+
+chrome.contextMenus.create(
+  {
+    title: 'Switch Off',
+    id: 'SWITCH_OFF',
+    contexts: ['all'],
+    type: 'normal'
+  },
+  (err, resp) => { console.log('Context Menu created successfully', err, resp); }
+);
+
+chrome.action.onClicked.addListener(
+  () => {
+    console.log('Stopping the extension');
+    chrome.action.setBadgeText(
+      { text: "OFF" },
+      (err, resp) => { console.log({err, resp}) }
+    );
+  }
+)
--- www/src/config/constants.js
+++ www/src/config/constants.js
@@ -1,4 +1,5 @@
 const constants = {
-  SOCKET_URL: 'https://wifishare-production.up.railway.app/'
+  SOCKET_URL: 'http://localhost:5000/'
+  // SOCKET_URL: 'https://wifishare-production.up.railway.app/'
 }
 export default constants;
