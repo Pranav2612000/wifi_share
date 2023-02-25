427c3be4edfb05fa23bea96a5be01a1af721aa60
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-25 21:03:10 +0530

    wip: add logic to check if app is enabled or not in react

--- www/src/App.js
+++ www/src/App.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Scratchpad from './components/Scratchpad.jsx';
 import './App.css';
-import DiscoveryService from './service/Discovery';
+import {isExtension, getValueFromChromeStorage} from './service/Extension';
 
 function App() {
+
+  useEffect(() => {
+    const getAppState = async () => {
+      if (isExtension) {
+        const enabled = await getValueFromChromeStorage('enabled');
+        console.log({ enabled });
+      }
+    };
+
+    getAppState();
+  }, []);
+
   return (
     <div className="App">
       <Header />
