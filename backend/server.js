7d6c4a3044a21eb7e4f9b825774d40d1f945f2c3
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-09 23:02:03 +0530

    feat: allow access to backend from everywhere using cors package

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -8,6 +9,7 @@ const getIpAddress = require('./helpers/getIpAddress');
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 app.set('trust proxy', true);
 
