000975d32bff980a7eef73e79376b2c3c806879a
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-03-30 08:21:44 +0530

    improv: extract client ip address even if user contains multiple X-forwarded-for

--- backend/helpers/getIpAddress.js
+++ backend/helpers/getIpAddress.js
@@ -1,3 +1,10 @@
-const getIpAddress = (socket) => socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
+const getIpAddress = (socket) => {
+  const ips = socket.handshake.headers['x-forwarded-for'];
+
+  if (!ips || ips.length === 0) {
+    return socket.request.connection.remoteAddress;
+  }
+  return ips.split(', ')[0];
+};
 
 module.exports = getIpAddress;
